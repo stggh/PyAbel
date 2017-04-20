1c02c7994602727212816b8a29814a672615ffab
Stephen Gibson <Stephen.Gibson@anu.edu.au>
2017-04-21 09:44:12 +1000

    Push to sync. Dropped testing section

--- abel/fourier.py
+++ abel/fourier.py
@@ -19,6 +19,7 @@ import matplotlib.gridspec as gridspec
 #  Z. Naturforsch 46a, 639-641 (1991)
 #
 # 2017-04 Stephen Gibson - python coded algorithm
+#         Dan Hickstein - code improvement
 #
 #############################################################################
 
@@ -66,6 +67,7 @@ def fourier_transform(IM, Nl=0, Nu=21, basis_dir='.', direction='inverse'):
 
     IM = np.atleast_2d(IM)
     rows, cols = IM.shape   # shape of input quadrant (or half image)
+    c2 = cols//2
 
     # coefficients of cosine series: f(r) = An (1 - (-1)^n cos(n pi r/R))
     # many coefficients An may provide a better fit, but creates more computation
@@ -78,9 +80,9 @@ def fourier_transform(IM, Nl=0, Nu=21, basis_dir='.', direction='inverse'):
     # array to hold the inverse Abel transform
     AIM = np.zeros_like(IM)
 
-    for rownum, IMrow in enumerate(IM):
+    for rownum, imrow in enumerate(IM):
         # fit basis to an image row
-        res = least_squares(residual, An, args=(IMrow, rownum, hbasis))
+        res = least_squares(residual, An, args=(imrow, rownum, hbasis))
 
         An = res.x  # store as initial guess for next row fit
 
@@ -89,13 +91,14 @@ def fourier_transform(IM, Nl=0, Nu=21, basis_dir='.', direction='inverse'):
         # evaluated with the row-fitted coefficients An
         AIM[rownum] = np.dot(An, fbasis)
 
-    return AIM
+    # tmp return coefficients An (for last processed row)
+    return AIM, An
 
 
-def residual(An, IMrow, rownum, Hbasis):
+def residual(An, imrow, rownum, Hbasis):
     # least-squares adjust coefficients An
     # difference between image row and the basis function
-    return IMrow - 2*np.dot(An, Hbasis[:, rownum])
+    return imrow - 2*np.dot(An, Hbasis[:, rownum])
 
 
 def f(r, R, n):
@@ -141,53 +144,3 @@ def _bs_fourier(N, rows, cols):
             hbasis[i, :, j] = h(j, R, n)
 
     return fbasis, hbasis
-
-
-# testing -----------------------------------
-if __name__ == "__main__":
-
-    # IM = abel.tools.analytical.sample_image(301, name='dribinski')
-    m = 301
-    m2 = m//2
-    x = np.linspace(-m2, m2, m)
-    y = np.linspace(-m2, m2, m)
-    XX, YY = np.meshgrid(x, y)
-    # 2D Gaussian intensity function at image centre
-    IM = np.exp(-(XX**2 + YY**2)/m2/10)
-
-    Q = abel.tools.symmetry.get_image_quadrants(IM)
-    Q0 = Q[0]  # top right side quadrant
-
-    # forward Abel transform
-    fQ0 = abel.hansenlaw.hansenlaw_transform(Q0, direction='forward')
-
-    # inverse Abel
-    AQ0 = fourier_transform(fQ0, Nl=0, Nu=31)
-
-    radial, speed = abel.tools.vmi.angular_integration(AQ0, origin=(0, 0))
-    orgradial, orgspeed = abel.tools.vmi.angular_integration(Q0,
-                                                             origin=(0, 0))
-
-    # plots ---------------------
-    fig = plt.figure()
-    gs = gridspec.GridSpec(2, 3)
-    ax0 = plt.subplot2grid((2, 3), (0, 0), colspan=3)
-    ax1 = plt.subplot2grid((2, 3), (1, 0))
-    ax2 = plt.subplot2grid((2, 3), (1, 1))
-    ax3 = plt.subplot2grid((2, 3), (1, 2))
-    ax1.imshow(Q0)
-    ax1.axis('off')
-    ax1.set_title('original')
-    ax2.imshow(fQ0)
-    ax2.axis('off')
-    ax2.set_title('forward Abel')
-    ax3.imshow(AQ0, vmin=0)
-    ax3.axis('off')
-    ax3.set_title('fourier inverse Abel')
-
-    ax0.plot(radial, speed/speed.max(), label='Fourier')
-    ax0.plot(orgradial, orgspeed/orgspeed.max(), zorder=0, label='original',
-             lw=3)
-    ax0.legend()
-    plt.savefig("plot_example_fourier.png", dpi=75)
-    plt.show()
